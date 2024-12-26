9fd05171c50d423b08df8b0a7236ede47291869b
ymj07168 <ymj07168@gmail.com>
2024-12-26 13:46:39 +0900

    spring practice init

---
+++ hellospring/src/main/java/hellospring/controller/MyAnnotation.java
@@ -0,0 +1,17 @@
+package hellospring.controller;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(CLASS)
+@Target({ TYPE, FIELD, METHOD, PARAMETER })
+public @interface MyAnnotation {
+	String value() default "";
+	int no() default 0;
+}
